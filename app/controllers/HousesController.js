57022eb6f75312ceb22bf1a9fc1952ec332481e2
Porter Young <porteryoung2005@gmail.com>
2024-06-25 15:31:25 -0600

    drawing houses to the screen

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,7 +1,25 @@
+import { AppState } from "../AppState.js";
 import { housesService } from "../services/HousesService.js";
+import { setHTML } from "../utils/Writer.js";
 
 export class HousesController {
     constructor() {
+        // AppState.on('houses', this.drawHouses)
         console.log('house controller init');
+        this.getHouses()
+    }
+
+    async getHouses() {
+        await housesService.getHouses()
+        this.drawHouses()
+    }
+
+    drawHouses() {
+        let houses = AppState.houses
+        let injectString
+        houses.forEach(house => {
+            injectString += house.htmlTemplate
+        });
+        setHTML('houses-area', injectString)
     }
 }
--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -5,20 +5,21 @@ import { api } from "./AxiosService.js";
 class HousesService {
     constructor() {
         console.log('Houses Service init');
-        this.getHouses()
+        // this.getHouses()
     }
 
     async getHouses() {
+        let response
         try {
-            const response = await api.get('api/houses')
+            response = await api.get('api/houses')
             console.log(response.data);
-            let houses = response.data.map(house => new House(house))
-            AppState.houses = houses
-            console.log(AppState.houses)
+
         } catch (error) {
             console.log('unable to load houses', error);
         }
-
+        let houses = response.data.map(house => new House(house))
+        AppState.houses = houses
+        console.log(AppState.houses)
 
     }
 }
